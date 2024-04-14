dc5b78abd2967e32da2dfc39036affc8e896b33e
Sebastián Remaggi Flores <macbook@Macbooks-MacBook-Pro.local>
2024-04-13 23:00:03 -0400

    I change the package route import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import { NavbarProvider } from '../contexts/NavbarContext'; // Asegúrate de que la ruta de importación sea correcta
 import Navbar from '@/components/navbar/Navbar';
-import Footer from '@/components/footer/Footer';
+import Footer from '@/components/footer/footer';
 import '../app/globals.css';
 
 function MyApp({ Component, pageProps }) {

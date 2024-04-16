65565855e84cf6438f3ae32eda3ddf5152fa726e
Sebastián Remaggi Flores <macbook@Macbooks-MacBook-Pro.local>
2024-04-15 23:10:23 -0400

    fix to update env bucket name 14

--- pages/editProjects.jsx
+++ pages/editProjects.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 
 function AdminPanel() {
-  console.log("ENVV", process.env.GCLOUD_STORAGE_BUCKET)
+  console.log("ENVV", process.env.GCP_STORAGE_BUCKET)
   const [projects, setProjects] = useState([]);
   const [selectedProjectId, setSelectedProjectId] = useState('');
   const [project, setProject] = useState({
